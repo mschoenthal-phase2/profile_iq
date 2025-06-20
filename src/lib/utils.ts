9997568b520de6e9de0c7a347c9acb8977b38dae
mschoenthal-phase2 <mschoenthal@phase2.io>
2025-06-20 13:00:13 -0500

    Update utils.ts

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,6 @@
 // src/lib/utils.ts
 import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-variants"
+import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
